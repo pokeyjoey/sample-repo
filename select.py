93ecbc76292b64d15803067b03cc103dc28f65ad
Jeffery Roche <jkeauctions@gmail.com>
2022-08-16 19:18:51 -0400

    Adding script for selecting with sql.

---
+++ select.py
@@ -0,0 +1,16 @@
+#import pandas as pd
+#import sqlite3
+
+# create our database
+#conn = sqlite3.connect('mean_green.db')
+
+# insert data into the table employee
+#url = "https://raw.githubusercontent.com/eng-6-22/mod-1-sql-curriculum/master/1-sql-fundamentals/7-sql-select/employees.csv"
+#df = pd.read_csv(url)
+#df.to_sql('employees', conn, index=False)
+
+
+#cursor = conn.cursor()
+
+#cursor.execute('SELECT * FROM employees;')
+#cursor.fetchall()
